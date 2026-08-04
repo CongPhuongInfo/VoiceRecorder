aec28418bfa9d79de063b7bd777b35896626725f
Công Phượng <congphuonginfo@gmail.com>
2026-08-04 13:23:50 +0700

    update

--- VoiceRecorder/obj/Release/net9.0-windows/VoiceRecorder.AssemblyInfo.vb
+++ VoiceRecorder/obj/Release/net9.0-windows/VoiceRecorder.AssemblyInfo.vb
@@ -15,7 +15,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("VoiceRecorder"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Release"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9312d0bf584fa97ed7a15640a97611307294d87b"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ee6b4f6e2f6c2e7b6f4de42a7f71656b42feb5"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("VoiceRecorder"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("VoiceRecorder"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
